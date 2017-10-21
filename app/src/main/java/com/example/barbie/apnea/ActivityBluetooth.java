5facd06a91af9e2383de0f301695711d965fb031
Barbie <barbiealmaraz>
2017-10-21 16:13:52 -0300

    Initial commit

---
+++ app/src/main/java/com/example/barbie/apnea/ActivityBluetooth.java
@@ -0,0 +1,13 @@
+package com.example.barbie.apnea;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityBluetooth extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth);
+    }
+}
