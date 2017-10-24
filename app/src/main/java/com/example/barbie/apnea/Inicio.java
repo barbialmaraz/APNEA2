bd7f23c59e0ff45cfc6e89cb17b3253d0d87d0e1
g.a.rodriguez <36822462>
2017-10-24 00:09:08 -0300

    Menu bar
    .Se agregaron las opciones en la parte de arriba

--- app/src/main/java/com/example/barbie/apnea/Inicio.java
+++ app/src/main/java/com/example/barbie/apnea/Inicio.java
@@ -1,7 +1,11 @@
 package com.example.barbie.apnea;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class Inicio extends AppCompatActivity {
 
@@ -9,8 +13,28 @@ public class Inicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_opciones, menu);
+        return true;
     }
 
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id==R.id.menuItemSync) {
+            Intent intent = new Intent(this, ActivityBluetooth.class);
+            this.startActivity(intent);
+        }
+        /*if (id==R.id.opcion2) {
+            Toast.makeText(this,"Se seleccionó la segunda opción",Toast.LENGTH_LONG).show();
+        }
+        if (id==R.id.opcion3) {
+            Toast.makeText(this,"Se seleccionó la tercer opción", Toast.LENGTH_LONG).show();
+        }*/
+        return super.onOptionsItemSelected(item);
+    }
 
 }
